d2d79b24d9935c974f5261f59e07a218e3e2b064
FatihIndraa <fatihindra140903@gmail.com>
2024-04-04 04:25:45 +0700

    up relative layout

--- app/src/main/java/com/example/layout/MainActivity.java
+++ app/src/main/java/com/example/layout/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.layout;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,36 +10,42 @@ import android.widget.Button;
 
 import com.example.layout.R;
 
-public class MainActivity extends AppCompatActivity {
-    private Button saveInternal;
-    private TextView nim,nama;
-    private TextView hnim,hnama;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button saveInternal, buttonPindah;
+    private EditText nim, nama;
+    private TextView hnim, hnama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nim=(TextView) findViewById(R.id.inputData);
-        nama=(TextView) findViewById(R.id.inputData1);
-        hnim=(TextView) findViewById(R.id.hasilNim);
-        hnama=(TextView) findViewById(R.id.hasilNama);
-        saveInternal=(Button) findViewById(R.id.saveInternal);
+
+        nim = findViewById(R.id.inputData);
+        nama = findViewById(R.id.inputData1);
+        hnim = findViewById(R.id.hasilNim);
+        hnama = findViewById(R.id.hasilNama);
+        saveInternal = findViewById(R.id.saveInternal);
+        buttonPindah = findViewById(R.id.buttonPindah);
+
         saveInternal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String xnim,xnama;
-                xnim=nim.getText().toString().trim();
-                xnama=nama.getText().toString().trim();
+                String xnim = nim.getText().toString().trim();
+                String xnama = nama.getText().toString().trim();
 
                 hnim.setText(xnim);
                 hnama.setText(xnama);
             }
         });
+
+        buttonPindah.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.buttonPindah) {
             Intent intent = new Intent(MainActivity.this, relative.class);
             startActivity(intent);
         }
+    }
 }
--- app/src/main/java/com/example/layout/relative.java
+++ app/src/main/java/com/example/layout/relative.java
@@ -1,24 +1,36 @@
 package com.example.layout;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class relative extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_relative);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button buttonLinear = findViewById(R.id.buttonlinear);
+        Button buttonWrap = findViewById(R.id.buttonconstraint);
+
+        buttonLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(relative.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
-    }
-}
+        buttonWrap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(relative.this, constraint.class);
+                startActivity(intent);
+            }
+        });
+
+
+}}
