a0ed782879c3c9bfe5415427690677ccdff73710
FatihIndraa <fatihindra140903@gmail.com>
2024-04-04 04:45:19 +0700

    up constraint

--- app/src/main/java/com/example/layout/constraint.java
+++ app/src/main/java/com/example/layout/constraint.java
@@ -8,29 +8,29 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class relative extends AppCompatActivity {
+public class constraint extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative);
+        setContentView(R.layout.activity_constraint);
 
-        Button buttonLinear = findViewById(R.id.buttonlinear);
-        Button buttonConstraint = findViewById(R.id.buttonconstraint);
+        Button buttonLinear = findViewById(R.id.button1);
+        Button buttonRelative = findViewById(R.id.button2);
 
         buttonLinear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(relative.this, MainActivity.class);
+                Intent intent = new Intent(constraint.this, MainActivity.class);
                 startActivity(intent);
             }
         });
-        buttonConstraint.setOnClickListener(new View.OnClickListener() {
+        buttonRelative.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(relative.this, constraint.class);
+                Intent intent = new Intent(constraint.this, relative.class);
                 startActivity(intent);
             }
         });
 
 
-}}
+    }}
