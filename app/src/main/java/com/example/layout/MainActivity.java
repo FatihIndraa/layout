246cf5db4f86c48cd7b641c3b438e8d6692eade3
FatihIndraa <fatihindra140903@gmail.com>
2024-04-04 03:55:53 +0700

    fix linear layout

--- app/src/main/java/com/example/layout/MainActivity.java
+++ app/src/main/java/com/example/layout/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.layout;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+
+import com.example.layout.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button saveInternal;
+    private TextView nim,nama;
+    private TextView hnim,hnama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        nim=(TextView) findViewById(R.id.inputData);
+        nama=(TextView) findViewById(R.id.inputData1);
+        hnim=(TextView) findViewById(R.id.hasilNim);
+        hnama=(TextView) findViewById(R.id.hasilNama);
+        saveInternal=(Button) findViewById(R.id.saveInternal);
+        saveInternal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String xnim,xnama;
+                xnim=nim.getText().toString().trim();
+                xnama=nama.getText().toString().trim();
+
+                hnim.setText(xnim);
+                hnama.setText(xnama);
+            }
         });
     }
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonPindah) {
+            Intent intent = new Intent(MainActivity.this, relative.class);
+            startActivity(intent);
+        }
 }
